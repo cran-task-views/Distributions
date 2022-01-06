fde383415e27d13403af56e6958b5e96c2d0f073
Christophe Dutang <dutangc@gmail.com>
2022-01-06 16:13:38 +0100

    small R code to check

---
+++ CompileVerif.R
@@ -0,0 +1,8 @@
+library("ctv")
+ctv2html("Distributions.md")
+browseURL("Distributions.html")
+
+#see help at https://github.com/cran-task-views/ctv/blob/main/Contributing.md
+
+check_ctv_packages("Distributions.md")
+
